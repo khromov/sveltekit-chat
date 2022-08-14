414ed56a346335b6f03b72087ba632ea48eb9825
Stanislav Khromov <stanislav+github@khromov.se>
2022-08-15 00:50:49 +0200

    Add name to signup, fix typescript hellscape

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,31 @@
+<script context="module" lang="ts">
+    import type { Load } from '@sveltejs/kit'
+  
+    export const load: Load = ({ session }) => {
+
+      if (!session.user) {
+        return {
+          status: 302,
+          redirect: '/auth/login',
+        }
+      }
+  
+      return {
+        status: 200,
+      }
+    }
+</script>
+
 <script lang="ts">
+    import { session } from '$app/stores'
     import ReconnectingEventSource from 'reconnecting-eventsource';
     import { v4 as uuidv4 } from 'uuid';
 
     import { onMount } from 'svelte';
     import Login from '$lib/components/Login.svelte';
     import { name } from '$lib/stores/preferences';
+
+    $: console.log($session);
 
     const uuid = uuidv4();
 
